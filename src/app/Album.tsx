7177353fa2414cdcd1f70edaacfc10cc0d1b9139
Alesh Houdek <mail@alesh.com>
2023-09-25 22:08:08 -0600

    update styling of rating and genre

--- src/app/Album.tsx
+++ src/app/Album.tsx
@@ -7,15 +7,16 @@ const baseUrl = "https://pitchfork.com"
 
 export default function Album({ album: input }: { album: AlbumType }) {
   const { tombstone, genres: genresEntity, url } = input
-  const { albums } = tombstone
+  const { albums, bnm, bnr } = tombstone
   const { album, rating: rtg } = albums?.[0] || {}
   const artist = album?.artists
     ?.map((artist) => artist.display_name)
     .join(" · ")
   const name = album?.display_name
   const rating = rtg?.display_rating.replace(".0", "")
-  const genres = genresEntity?.map((genre) => genre.display_name).join(", ")
+  const genres = genresEntity?.map((genre) => genre.display_name)
   const albumImageUrl = album?.photos.tout.sizes.homepageLarge
+  const isBest = bnm || bnr
 
   return (
     <a href={baseUrl + url} target="_blank" className={styles.album}>
@@ -33,15 +34,21 @@ export default function Album({ album: input }: { album: AlbumType }) {
         </div>
       )}
       <div className={styles.titleContainer}>
-        <div className={styles.rating}>{rating}</div>
+        <div className={`${styles.rating} ${isBest ? styles.bestNew : ""}`}>
+          {rating}
+        </div>
         <h2>
           <span className={styles.artist}>{artist}</span>
           <br />
           <span className={styles.name}>{name}</span>
         </h2>
       </div>
-      <p>
-        <strong>{genres}</strong>
+      <p className={styles.genreWrapper}>
+        {genres?.map((genre) => (
+          <span key={genre} className={styles.genre}>
+            {genre}
+          </span>
+        ))}
       </p>
     </a>
   )
