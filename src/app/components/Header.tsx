5cfd28c95fd186a92013a3e1ec9eb91d56f441b5
Alesh Houdek <mail@alesh.com>
2023-10-22 22:20:06 -0600

    add joy ui and a header slider

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,41 @@
 "use client"
+import { useState } from "react"
 import { useParams } from "next/navigation"
+import Slider from "@mui/joy/Slider"
 import styles from "./Header.module.css"
 
+// https://mui.com/joy-ui/react-slider/
+
 export default function Header({ text = "" }) {
+  const [rating, setRating] = useState(0)
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    console.log(newValue)
+    setRating(newValue as number)
+  }
+
   return (
     <nav className={styles.wrapper}>
-      <a href="/">Forker</a> {text}
+      <div style={{ width: 200 }}>
+        <a href="/">Forker</a> {text}
+      </div>
+
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div>Rating</div>
+        <div style={{ width: 200 }}>
+          <Slider
+            step={0.1}
+            value={rating}
+            max={10}
+            onChange={handleChange}
+            color="neutral"
+            track="inverted"
+            size="sm"
+            variant="solid"
+          />
+        </div>
+        <div>{rating} – 10</div>
+      </div>
     </nav>
   )
 }
