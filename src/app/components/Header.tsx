70a0031b807e8f415de2970844cae17c21ddf340
Alesh Houdek <mail@alesh.com>
2023-10-23 21:22:59 -0600

    add modal and genre checkboxes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,41 +1,16 @@
 "use client"
 import { useState } from "react"
-import { useParams } from "next/navigation"
-import Slider from "@mui/joy/Slider"
 import styles from "./Header.module.css"
-
-// https://mui.com/joy-ui/react-slider/
+import OptionsModal from "./OptionsModal"
 
 export default function Header({ text = "" }) {
-  const [rating, setRating] = useState(0)
-
-  const handleChange = (event: Event, newValue: number | number[]) => {
-    console.log(newValue)
-    setRating(newValue as number)
-  }
-
   return (
     <nav className={styles.wrapper}>
       <div style={{ width: 200 }}>
         <a href="/">Forker</a> {text}
       </div>
 
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <div>Rating</div>
-        <div style={{ width: 200 }}>
-          <Slider
-            step={0.1}
-            value={rating}
-            max={10}
-            onChange={handleChange}
-            color="neutral"
-            track="inverted"
-            size="sm"
-            variant="solid"
-          />
-        </div>
-        <div>{rating} – 10</div>
-      </div>
+      <OptionsModal />
     </nav>
   )
 }
---
+++ src/app/components/OptionsModal.tsx
@@ -0,0 +1,108 @@
+import { useState } from "react"
+import Button from "@mui/joy/Button"
+import Modal from "@mui/joy/Modal"
+import ModalClose from "@mui/joy/ModalClose"
+import Slider from "@mui/joy/Slider"
+import Sheet from "@mui/joy/Sheet"
+import Checkbox from "@mui/joy/Checkbox"
+import List from "@mui/joy/List"
+import ListItem from "@mui/joy/ListItem"
+
+// https://mui.com/joy-ui/react-slider/
+
+const genres: Record<string, string> = {
+  electronic: "Electronic",
+  experimental: "Experimental",
+  folk: "Folk/Country",
+  global: "Global",
+  jazz: "Jazz",
+  metal: "Metal",
+  pop: "Pop/R&B",
+  rap: "Rap/Hip-Hop",
+  rock: "Rock",
+}
+
+function getInitialGenres(): Record<string, boolean> {
+  return Object.fromEntries(Object.keys(genres).map((genre) => [genre, true]))
+}
+
+function Genres() {
+  const [selected, setSelected] = useState(getInitialGenres())
+
+  function handleChange(event: React.ChangeEvent) {
+    const target = event.target as HTMLInputElement
+    const { value, checked } = target
+    const newSelection = { ...selected }
+    newSelection[value] = checked
+    setSelected(newSelection)
+  }
+
+  return (
+    <List>
+      {Object.keys(genres).map((genre) => (
+        <ListItem key={genre}>
+          <Checkbox
+            onChange={handleChange}
+            label={genres[genre]}
+            value={genre}
+            checked={selected[genre]}
+          />
+        </ListItem>
+      ))}
+    </List>
+  )
+}
+
+export default function OptionsModal() {
+  const [open, setOpen] = useState<boolean>(false)
+  const [rating, setRating] = useState<number>(0)
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    console.log(newValue)
+    setRating(newValue as number)
+  }
+  return (
+    <>
+      {/* <Button variant="plain" color="neutral" onClick={() => setOpen(true)}>
+        Menu
+      </Button> */}
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            maxWidth: 500,
+            borderRadius: "md",
+            p: 3,
+            boxShadow: "lg",
+          }}
+        >
+          <ModalClose variant="plain" sx={{ m: 1 }} />
+          <Genres />
+
+          <div>
+            <div>Rating</div>
+            <div style={{ width: 200 }}>
+              <Slider
+                step={0.1}
+                value={rating}
+                max={10}
+                onChange={handleChange}
+                color="neutral"
+                track="inverted"
+                size="sm"
+                variant="solid"
+              />
+            </div>
+            <div>{rating} – 10</div>
+          </div>
+        </Sheet>
+      </Modal>
+    </>
+  )
+}
