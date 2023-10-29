3ba5e84cc3a930827609b8f10722d0b98a94136c
Alesh Houdek <mail@alesh.com>
2023-10-28 21:13:19 -0600

    refactor fetch and opt out of caching

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,28 +1,30 @@
-import devAlbums from "../../../dev-albums"
+// import devAlbums from "../../../dev-albums"
 import { Album } from "../types"
 
-const dev = process.env.NODE_ENV === "development"
+// const dev = process.env.NODE_ENV === "development"
 
 const baseUrl = "https://pitchfork.com/api/v2/"
 
-export async function getRecentReviews(): Promise<Album[]> {
-  if (dev) {
-    return devAlbums
-  }
-
-  const response = await fetch(
-    `${baseUrl}search/?types=reviews&hierarchy=sections%2Freviews%2Falbums%2Cchannels%2Freviews%2Falbums&sort=publishdate%20desc%2Cposition%20asc&size=200&start=0`
-  )
+async function pitchforkFetch(query: string): Promise<Album[]> {
+  const response = await fetch(baseUrl + query, { cache: "no-store" })
   const json = await response.json()
   return json.results.list
 }
 
-export async function getAlbumsByArtist(artistSlug: string): Promise<Album[]> {
-  const response = await fetch(
-    `${baseUrl}entities/artists/${artistSlug}/albumreviews/?size=200&start=0`
+export async function getRecentReviews(): Promise<Album[]> {
+  // if (dev) {
+  //   return devAlbums
+  // }
+
+  return await pitchforkFetch(
+    `search/?types=reviews&hierarchy=sections%2Freviews%2Falbums%2Cchannels%2Freviews%2Falbums&sort=publishdate%20desc%2Cposition%20asc&size=200&start=0`
   )
-  const json = await response.json()
-  return json.results.list
+}
+
+export async function getAlbumsByArtist(artistSlug: string): Promise<Album[]> {
+  return await pitchforkFetch(
+    `entities/artists/${artistSlug}/albumreviews/?size=200&start=0`
+  )
 }
 
 export async function getAlbumsByGenre(genres: string[]): Promise<Album[]> {
@@ -30,9 +32,7 @@ export async function getAlbumsByGenre(genres: string[]): Promise<Album[]> {
     .map((genre: string) => `genre=${genre.split("-")[0]}&`)
     .join("")
 
-  const response = await fetch(
-    `${baseUrl}search/?${query}types=reviews&hierarchy=sections/reviews/albums,channels/reviews/albums&sort=publishdate desc,position asc&size=200&start=0`
+  return await pitchforkFetch(
+    `search/?${query}types=reviews&hierarchy=sections/reviews/albums,channels/reviews/albums&sort=publishdate desc,position asc&size=200&start=0`
   )
-  const json = await response.json()
-  return json.results.list
 }
