af3341affeb28b849168c79ec408a75f5d3422ae
Alesh Houdek <mail@alesh.com>
2023-10-30 23:19:14 -0600

    save modal preferences to URL and load them back in on mount

--- src/app/components/OptionsModal.tsx
+++ src/app/components/OptionsModal.tsx
@@ -1,4 +1,11 @@
-import { useState, ChangeEvent } from "react"
+"use client"
+import { useCallback, useEffect, useState, ChangeEvent } from "react"
+import {
+  usePathname,
+  useSearchParams,
+  useRouter,
+  ReadonlyURLSearchParams,
+} from "next/navigation"
 import styles from "./OptionsModal.module.css"
 // eslint-disable-next-line no-unused-vars
 import Button from "@mui/joy/Button"
@@ -7,8 +14,6 @@ import Slider from "@mui/joy/Slider"
 import Sheet from "@mui/joy/Sheet"
 import Checkbox from "@mui/joy/Checkbox"
 import ListItem from "@mui/joy/ListItem"
-
-// https://mui.com/joy-ui/react-slider/
 
 const genres: Record<string, string> = {
   electronic: "Electronic",
@@ -26,43 +31,75 @@ function getInitialGenres(): Record<string, boolean> {
   return Object.fromEntries(Object.keys(genres).map((genre) => [genre, true]))
 }
 
-function Genres() {
-  const [selected, setSelected] = useState(getInitialGenres())
+function createQueryString(
+  searchParams: ReadonlyURLSearchParams,
+  genres: string[],
+  minRating: number
+) {
+  const params = new URLSearchParams()
+  genres.forEach((genre) => {
+    params.append("genre", genre)
+  })
+  params.set("minRating", minRating.toString())
 
-  function handleChange(event: ChangeEvent) {
-    const target = event.target as HTMLInputElement
-    const { value, checked } = target
-    const newSelection = { ...selected }
-    newSelection[value] = checked
-    setSelected(newSelection)
-  }
-
-  return (
-    <ul className={styles.checkboxContainer}>
-      {Object.keys(genres).map((genre) => (
-        <ListItem key={genre}>
-          <Checkbox
-            className={styles.checkbox}
-            color="neutral"
-            onChange={handleChange}
-            label={genres[genre]}
-            value={genre}
-            checked={selected[genre]}
-          />
-        </ListItem>
-      ))}
-    </ul>
-  )
+  return params.toString()
 }
 
 export default function OptionsModal() {
   const [open, setOpen] = useState<boolean>(false)
+  const [selectedGenres, setSelectedGenres] = useState<Record<string, boolean>>(
+    getInitialGenres()
+  )
   const [rating, setRating] = useState<number>(0)
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const router = useRouter()
 
-  const handleChange = (event: Event, newValue: number | number[]) => {
-    console.log(newValue)
+  const getUrlParameters = useCallback(
+    function getUrlParameters() {
+      const minRating: number = Number(searchParams.get("minRating"))
+      const urlGenres = searchParams.getAll("genre")
+      const genreValues = Object.fromEntries(
+        Object.keys(genres).map((genre) => [genre, urlGenres.includes(genre)])
+      )
+      setRating(minRating)
+      setSelectedGenres(genreValues)
+    },
+    [searchParams]
+  )
+
+  useEffect(() => {
+    getUrlParameters()
+  }, [getUrlParameters])
+
+  function handleSetGenres(event: ChangeEvent) {
+    const target = event.target as HTMLInputElement
+    const { value, checked } = target
+    const newSelection = { ...selectedGenres }
+    newSelection[value] = checked
+    setSelectedGenres(newSelection)
+  }
+
+  function handleSetRating(event: Event, newValue: number | number[]) {
     setRating(newValue as number)
   }
+
+  function handleCancel() {
+    getUrlParameters()
+    setOpen(false)
+  }
+
+  function handleSubmit() {
+    const genres = Object.keys(selectedGenres).filter(
+      (genre) => selectedGenres[genre]
+    )
+
+    router.replace(
+      pathname + "?" + createQueryString(searchParams, genres, rating)
+    )
+    setOpen(false)
+  }
+
   return (
     <>
       {/* <Button variant="plain" color="neutral" onClick={() => setOpen(true)}>
@@ -77,17 +114,29 @@ export default function OptionsModal() {
       >
         <Sheet className={styles.dialog}>
           <h3>Genres</h3>
-          <Genres />
+          <ul className={styles.checkboxContainer}>
+            {Object.keys(genres).map((genre) => (
+              <ListItem key={genre}>
+                <Checkbox
+                  className={styles.checkbox}
+                  color="neutral"
+                  onChange={handleSetGenres}
+                  label={genres[genre]}
+                  value={genre}
+                  checked={selectedGenres[genre]}
+                />
+              </ListItem>
+            ))}
+          </ul>
 
           <div className={styles.ratingWrapper}>
             <h3>Rating</h3>
-
             <div>
               <Slider
                 step={0.1}
                 value={rating}
                 max={10}
-                onChange={handleChange}
+                onChange={handleSetRating}
                 color="neutral"
                 track="inverted"
                 size="sm"
@@ -96,11 +145,19 @@ export default function OptionsModal() {
             </div>
             <div>{rating} – 10</div>
           </div>
+
           <div className={styles.buttonGroup}>
-            <Button color="neutral" variant="plain" className="button">
+            <Button
+              onClick={handleCancel}
+              color="neutral"
+              variant="plain"
+              className="button"
+            >
               Cancel
             </Button>
-            <Button color="neutral">Filter</Button>
+            <Button onClick={handleSubmit} color="neutral">
+              Filter
+            </Button>
           </div>
         </Sheet>
       </Modal>
