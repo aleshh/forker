ec75a92b33dd9476cbf504d4d6a1e43e45ac9c3c
Alesh Houdek <mail@alesh.com>
2023-12-03 11:22:00 -0700

    dark mode initial pass

--- src/app/search/[searchString]/page.tsx
+++ src/app/search/[searchString]/page.tsx
@@ -16,20 +16,16 @@ export default async function Search({
   const matchString = searchString.replaceAll("%20", " ").toLowerCase()
   const artists = await getSearchResults(matchString)
 
-  // const matchString = searchString.toLowerCase()
-
   const artist = (function findArtist() {
     console.log("searching for", matchString)
     const exactMatch = artists.find(
       (artist: Artist) => artist.name.toLowerCase() === matchString
     )
-    // if (exactMatch) console.log("exactMatch", exactMatch.url)
     if (exactMatch) return exactMatch
 
     const includesMatch = artists.find((artist: Artist) =>
       artist.name.toLowerCase().includes(matchString)
     )
-    // if (includesMatch) console.log("includesMatch", includesMatch.url)
     if (includesMatch) return includesMatch
 
     return null
@@ -39,13 +35,19 @@ export default async function Search({
     redirect(artist.url)
   }
 
-  console.log(artists)
-  // const genreName = getGenreName(albums[0], params.genres[0])
-
   return (
     <>
       <Header pageType="Search" pageName={searchString} />
-      No results for {searchString}
+      <div
+        style={{
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p>No results for &quot;{searchString}&quot;</p>
+      </div>
     </>
   )
 }
