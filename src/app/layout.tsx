3bbf0ab530262355300c17b8991700dc3102274b
Alesh Houdek <mail@alesh.com>
2023-12-17 22:10:04 -0700

    fix dark mode for modals and other joy components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react"
+import { CssVarsProvider } from "@mui/joy/styles"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
@@ -18,7 +19,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <CssVarsProvider defaultMode="system">
+        <body className={inter.className}>{children}</body>
+      </CssVarsProvider>
     </html>
   )
 }
